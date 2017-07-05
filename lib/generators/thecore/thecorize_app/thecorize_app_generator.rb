6b244d6fd8d0df79a9039ea833377c4ae5743f22
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2017-07-05 23:06:50 +0200

    Debug thecorize_app

--- lib/generators/thecore/thecorize_app/thecorize_app_generator.rb
+++ lib/generators/thecore/thecorize_app/thecorize_app_generator.rb
@@ -3,11 +3,6 @@ module Thecore
   class ThecorizeAppGenerator < Rails::Generators::NamedBase
     source_root File.expand_path('../templates', __FILE__)
 
-    def setup_variables
-      @plugin_path = @destination_stack.first.match(Regexp.new("^.*#{@name}"))[0]
-      @plugin_parent_name = @plugin_path.split(File::SEPARATOR).last
-    end
-
     def remove_index
       #remove the index.html
       remove_file 'public/index.html'
