407f0f89ed207fe04e15a5abc12d491e3f7a24a1
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2017-10-19 12:30:55 +0200

    Added jquery ui

--- app/assets/javascripts/thecore.js
+++ app/assets/javascripts/thecore.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery-ui
+//= require jquery-ui/dialog
 //= require jquery_ujs
 //= require bootstrap
 //= require ie
--- lib/thecore/version.rb
+++ lib/thecore/version.rb
@@ -1,3 +1,3 @@
 module Thecore
-  VERSION = "1.3.13"
+  VERSION = "1.3.16"
 end
